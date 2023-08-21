ea502c95d50c56eaf0a98d737f62ad7ab24e8a4e
Kimgmltn <Kimgmltn0323@gmail.com>
2023-08-21 22:37:28 +0900

    트랜잭션 문제해결-트랜잭션 AOP 적용

--- src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -1,39 +1,25 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV2;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 매니저
+ * 트랜잭션 - @Transactional AOP
  */
-@RequiredArgsConstructor
 @Slf4j
-public class MemberServiceV3_1 {
+@RequiredArgsConstructor
+public class MemberServiceV3_3 {
 
-    private final PlatformTransactionManager transactionManager;
     private final MemberRepositoryV3 memberRepository;
 
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        // 트랜잭션 시작
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        try {
-            // 비즈니스 로직
-            bizLogic(fromId, toId, money);
-            transactionManager.commit(status); // 성공시 커밋
-        } catch (Exception e) {
-            transactionManager.rollback(status);
-            throw new IllegalStateException(e);
-        }
+        bizLogic(fromId, toId, money);
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
